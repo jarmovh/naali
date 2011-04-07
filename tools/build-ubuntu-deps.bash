36308c44e9443503bb54cf10f807fdd2bd952102
Jarmo Vähä <jarmovh@gmail.com>
2011-04-07 10:10:03 +0300

    Small natty fix in build-ubuntu-deps.

--- tools/build-ubuntu-deps.bash
+++ tools/build-ubuntu-deps.bash
@@ -40,6 +40,7 @@ export CXX="ccache g++"
 export CCACHE_DIR=$deps/ccache
 
 if lsb_release -c | grep -q natty; then
+	sudo apt-get install aptitude
 	sudo aptitude -y install scons python-dev libogg-dev libvorbis-dev \
 	 libopenjpeg-dev libcurl4-gnutls-dev libexpat1-dev libphonon-dev \
 	 build-essential g++ libogre-dev libboost-all-dev libpoco-dev \
