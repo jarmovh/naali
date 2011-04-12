47d8fc5a629daa5e0a54a358bc9a4a240c2a17ce
Jarmo Vähä <jarmovh@gmail.com>
2011-04-12 09:40:31 +0300

    Some changes.

--- tools/build-fedora-deps.bash
+++ tools/build-fedora-deps.bash
@@ -32,6 +32,11 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 export CCACHE_DIR=$deps/ccache
 
+yum install scons libogg-devel python-devel libvorbis-devel openjpeg-devel \
+libcurl-devel expat-devel phonon-devel ogre-devel boost-devel poco-devel \
+pygtk2-devel dbus-devel ccache qt-devel telepathy-farsight-devel libnice-devel \
+bison flex libxml2-devel ois-devel cmake freealut-devel
+
 function build-regular {
     urlbase=$1
     shift
@@ -78,17 +83,17 @@ fi
 
 cd $build
 what=PythonQt
+ver=2.0.1
 if test -f $tags/$what-done; then
     echo $what is done
 else
-    if test -d $what; then
-        svn update $what
-    else
-        svn co $viewerdeps_svn/trunk/$what $what
-    fi
-    cd $what
-    /usr/lib/qt4/bin/qmake
-    make
+    rm -rf $what$ver
+    zip=../tarballs/$what$ver.zip
+    test -f $zip || wget -O $zip http://downloads.sourceforge.net/project/pythonqt/pythonqt/$what-$ver/$what$ver.zip
+    unzip $zip
+    cd $what$ver
+    qmake
+    make -j2
     rm -f $prefix/lib/lib$what*
     cp -a lib/lib$what* $prefix/lib/
     cp src/PythonQt*.h $prefix/include/
